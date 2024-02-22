8e5af5f1744a5927754df32bf126a8771ac27d84
zqh <1846508601@qq.com>
2024-02-22 15:40:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-config-provider :locale="locale">
+      <RouterView />
+    </el-config-provider>
+
+</template>
+<script setup>
+import {  RouterView } from 'vue-router'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const locale = zhCn
+</script>
+<style scoped>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-pagination
+      v-model:current-page="currentPage1"
+      :page-size="100"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, prev, pager, next"
+      :total="1000"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+    <el-button type="primary">登录</el-button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+</style>
